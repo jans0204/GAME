b350001da1cd1ceac33d20fc635f952ba6ed1233
jans0204 <111557645+jans0204@users.noreply.github.com>
2023-07-19 13:20:03 +0800

    Add files via upload

---
+++ DOS(1886)/HardLink.bat
@@ -0,0 +1,225 @@
+:: md _Games(hardlink)
+:: cd _Games(hardlink)
+
+mklink /H 0004_ALONE3.rar       ..\DOS.Part001.rar
+
+mklink /H alpha-1.rar           ..\DOS.Part001.rar
+mklink /H alpha-2.rar           ..\DOS.Part002.rar
+
+mklink /H ALQADIM_atria.rar     ..\DOS.Part002.rar
+
+mklink /H AUTODUEL_bintime.rar  ..\DOS.Part003.rar
+
+mklink /H bioforge-1.rar        ..\DOS.Part003.rar
+mklink /H bioforge-2.rar        ..\DOS.Part004.rar
+
+mklink /H biom_brutal.rar       ..\DOS.Part004.rar
+
+mklink /H bs-1.rar              ..\DOS.Part004.rar
+mklink /H bs-2.rar              ..\DOS.Part005.rar
+
+mklink /H bsd_cmaze.rar         ..\DOS.Part005.rar
+
+mklink /H CMDJ2-1.rar           ..\DOS.Part005.rar
+mklink /H CMDJ2-2.rar           ..\DOS.Part006.rar
+
+mklink /H cmworldd_CQZ.rar      ..\DOS.Part006.rar
+
+mklink /H crazy-1.rar           ..\DOS.Part006.rar
+mklink /H crazy-2.rar           ..\DOS.Part007.rar
+
+mklink /H CRAZYD_cyril.rar      ..\DOS.Part007.rar
+
+mklink /H dagger-1.rar          ..\DOS.Part007.rar
+mklink /H dagger-2.rar          ..\DOS.Part008.rar
+
+mklink /H DAMIN_DIF2.rar        ..\DOS.Part008.rar
+
+mklink /H DIG-1.rar             ..\DOS.Part008.rar
+mklink /H DIG-2.rar             ..\DOS.Part009.rar
+
+mklink /H digger2_DOR.rar       ..\DOS.Part009.rar
+
+mklink /H DOT_ECSTATIC.rar      ..\DOS.Part010.rar
+
+mklink /H ECSTATIC2-1.rar       ..\DOS.Part010.rar
+mklink /H ECSTATIC2-2.rar       ..\DOS.Part011.rar
+
+mklink /H ED1.rar               ..\DOS.Part011.rar
+
+mklink /H ED2-1.rar             ..\DOS.Part011.rar
+mklink /H ED2-2.rar             ..\DOS.Part012.rar
+
+mklink /H ED3_f2b.rar           ..\DOS.Part012.rar
+
+mklink /H FABLE-1.rar           ..\DOS.Part012.rar
+mklink /H FABLE-2.rar           ..\DOS.Part013.rar
+
+mklink /H FACES_fr1.rar         ..\DOS.Part013.rar
+
+mklink /H fragile-1.rar         ..\DOS.Part013.rar
+mklink /H fragile-2.rar         ..\DOS.Part014.rar
+
+mklink /H framed_G2.rar         ..\DOS.Part014.rar
+
+mklink /H G4S-1.rar             ..\DOS.Part014.rar
+mklink /H G4S-2.rar             ..\DOS.Part015.rar
+
+mklink /H GABRIEL_HAPPY.rar     ..\DOS.Part015.rar
+
+mklink /H HAPPYCT-1.rar         ..\DOS.Part015.rar
+mklink /H HAPPYCT-2.rar         ..\DOS.Part016.rar
+
+mklink /H HAPPYMJ_HERO.rar      ..\DOS.Part016.rar
+
+mklink /H hero2-1.rar           ..\DOS.Part016.rar
+mklink /H hero2-2.rar           ..\DOS.Part017.rar
+
+mklink /H HEROES_HITBALL.rar    ..\DOS.Part017.rar
+
+mklink /H HJBZ-1.rar            ..\DOS.Part017.rar
+mklink /H HJBZ-2.rar            ..\DOS.Part018.rar
+
+mklink /H HL_HOLMES2.rar        ..\DOS.Part018.rar
+
+mklink /H HolyI-1.rar           ..\DOS.Part018.rar
+mklink /H HolyI-2.rar           ..\DOS.Part019.rar
+
+mklink /H homey_IDMAS.rar       ..\DOS.Part019.rar
+
+mklink /H IEYASU-1.rar          ..\DOS.Part019.rar
+mklink /H IEYASU-2.rar          ..\DOS.Part020.rar
+
+mklink /H IF1_IOD.rar           ..\DOS.Part020.rar
+
+mklink /H ironbloo-1.rar        ..\DOS.Part020.rar
+mklink /H ironbloo-2.rar        ..\DOS.Part021.rar
+
+mklink /H ironcros_KK10.rar     ..\DOS.Part021.rar
+
+mklink /H KKND-1.rar            ..\DOS.Part021.rar
+mklink /H KKND-2.rar            ..\DOS.Part022.rar
+
+mklink /H klass999_LBA.rar      ..\DOS.Part022.rar
+
+mklink /H LBA2-1.rar            ..\DOS.Part022.rar
+mklink /H LBA2-2.rar            ..\DOS.Part023.rar
+
+mklink /H LC_LIONKING.rar       ..\DOS.Part023.rar
+
+mklink /H LIUBAE-1.rar          ..\DOS.Part023.rar
+mklink /H LIUBAE-2.rar          ..\DOS.Part024.rar
+
+mklink /H lland_LUDIN.rar       ..\DOS.Part024.rar
+
+mklink /H LUNA2-1.rar           ..\DOS.Part024.rar
+mklink /H LUNA2-2.rar           ..\DOS.Part025.rar
+
+mklink /H lure_MCSPACE.rar      ..\DOS.Part025.rar
+
+mklink /H MDK-1.rar             ..\DOS.Part025.rar
+mklink /H MDK-2.rar             ..\DOS.Part026.rar
+
+mklink /H MDWVGA_N3D.rar        ..\DOS.Part026.rar
+
+mklink /H NANA-1.rar            ..\DOS.Part026.rar
+mklink /H NANA-2.rar            ..\DOS.Part027.rar
+
+mklink /H NANA2_obitus.rar      ..\DOS.Part027.rar
+
+mklink /H ocean-1.rar           ..\DOS.Part027.rar
+mklink /H ocean-2.rar           ..\DOS.Part028.rar
+
+mklink /H off_origamo.rar       ..\DOS.Part028.rar
+
+mklink /H orion-1.rar           ..\DOS.Part028.rar
+mklink /H orion-2.rar           ..\DOS.Part029.rar
+
+mklink /H ORION1_pbw.rar        ..\DOS.Part029.rar
+
+mklink /H PCE-1.rar             ..\DOS.Part029.rar
+mklink /H PCE-2.rar             ..\DOS.Part030.rar
+
+mklink /H PCRALLY_qt.rar        ..\DOS.Part030.rar
+
+mklink /H QUAKE-1.rar           ..\DOS.Part030.rar
+mklink /H QUAKE-2.rar           ..\DOS.Part031.rar
+
+mklink /H quandary_redcat2.rar  ..\DOS.Part031.rar
+
+mklink /H REDNECK-1.rar         ..\DOS.Part031.rar
+mklink /H REDNECK-2.rar         ..\DOS.Part032.rar
+
+mklink /H redneck2_sandstor.rar ..\DOS.Part032.rar
+
+mklink /H sandwar-1.rar         ..\DOS.Part032.rar
+mklink /H sandwar-2.rar         ..\DOS.Part033.rar
+
+mklink /H SANGO_SGYJ.rar        ..\DOS.Part033.rar
+
+mklink /H SGYY2-1.rar           ..\DOS.Part033.rar
+mklink /H SGYY2-2.rar           ..\DOS.Part034.rar
+
+mklink /H SH_skunkart.rar       ..\DOS.Part034.rar
+
+mklink /H SKY-1.rar             ..\DOS.Part034.rar
+mklink /H SKY-2.rar             ..\DOS.Part035.rar
+
+mklink /H skyroads_spelcros.rar ..\DOS.Part035.rar
+
+mklink /H SPELLC-1.rar          ..\DOS.Part035.rar
+mklink /H SPELLC-2.rar          ..\DOS.Part036.rar
+
+mklink /H spider_ST.rar         ..\DOS.Part036.rar
+
+mklink /H STALKER-1.rar         ..\DOS.Part036.rar
+mklink /H STALKER-2.rar         ..\DOS.Part037.rar
+
+mklink /H STAR_suflan.rar       ..\DOS.Part037.rar
+
+mklink /H SUIHO-1.rar           ..\DOS.Part037.rar
+mklink /H SUIHO-2.rar           ..\DOS.Part038.rar
+
+mklink /H SUMMER_SW.rar         ..\DOS.Part038.rar
+
+mklink /H SWARRIOR-1.rar        ..\DOS.Part038.rar
+mklink /H SWARRIOR-2.rar        ..\DOS.Part039.rar
+
+mklink /H swars_thelastk.rar    ..\DOS.Part039.rar
+
+mklink /H themep-1.rar          ..\DOS.Part039.rar
+mklink /H themep-2.rar          ..\DOS.Part040.rar
+
+mklink /H thepatri_tubular.rar  ..\DOS.Part040.rar
+
+mklink /H TUNTOWN-1.rar         ..\DOS.Part040.rar
+mklink /H TUNTOWN-2.rar         ..\DOS.Part041.rar
+
+mklink /H TURRICAN_vex.rar      ..\DOS.Part041.rar
+
+mklink /H VG-1.rar              ..\DOS.Part041.rar
+mklink /H VG-2.rar              ..\DOS.Part042.rar
+
+mklink /H VGOLF_WATER.rar       ..\DOS.Part042.rar
+
+mklink /H WATER1-1.rar          ..\DOS.Part042.rar
+mklink /H WATER1-2.rar          ..\DOS.Part043.rar
+
+mklink /H WATER2_wlfsbane.rar   ..\DOS.Part043.rar
+
+mklink /H WLONG-1.rar           ..\DOS.Part043.rar
+mklink /H WLONG-2.rar           ..\DOS.Part044.rar
+
+mklink /H wman_XCOM2.rar        ..\DOS.Part044.rar
+
+mklink /H xcoma-1.rar           ..\DOS.Part044.rar
+mklink /H xcoma-2.rar           ..\DOS.Part045.rar
+
+mklink /H xcomapo_YESHG.rar     ..\DOS.Part045.rar
+
+mklink /H YFZ-1.rar             ..\DOS.Part045.rar
+mklink /H YFZ-2.rar             ..\DOS.Part046.rar
+
+mklink /H YG_zzt.rar            ..\DOS.Part046.rar
+
+:: END (#46+43*2=132.rars)
